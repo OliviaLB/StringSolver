2ce0f8c30268d625b71e4668262c17b03f58ef69
OliviaLB <olivia.l.bates@gmail.com>
2024-09-06 17:24:32 +0100

    Implement initial logic for: Empty string Single value "1,2" Multiple values Comma delimiter

--- StringSolver/StringCalculator.cs
+++ StringSolver/StringCalculator.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace Technical
 {
     public class StringCalculator
@@ -11,7 +13,28 @@ namespace Technical
         /// <returns>The sum of all numbers in the string. Returns 0 if the input is empty or null.</returns>
         public static int Add(string numbers)
         {
-            return 0;
+            if (string.IsNullOrWhiteSpace(numbers))
+                return 0;
+
+            // Potential delimiters
+            char[] delimiters = { ','};
+            
+            int total = 0;
+
+            foreach (var item in numbers.Split(delimiters))
+            {
+                // Try parsing the value into an integer
+                if (int.TryParse(item, out int number))
+                {
+                    total += number;
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid value: {item}");
+                }
+            }
+
+            return total;
         }
     }
 }
